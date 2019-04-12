b9e8da75edb2c2f5659da9c3b13d1e144d7bdd18
jordanbyrne1996 <C15314056@mydit.ie>
2019-04-12 16:47:57 +0100

    ???

--- CompanyHandbookSoftwareQuality/src/Indentation.java
+++ CompanyHandbookSoftwareQuality/src/Indentation.java
@@ -1,17 +1,11 @@
-
-public class Indentation {
-
+public class Indentation { 
+	
 	// this is an example of how we DO NOT use indentation
 	public boolean typeOne() {
 		int x = 0;
 		int y = 0;
 		int z = 0;
-		if (x < 17 && y < 22 || z < 60) {
-			return true;
-		} else {
-			System.out.println("incorrect");
-			return false;
-		}
+		if (x < 17 && y < 22 || z < 60) {return true;} else {System.out.println("incorrect");return false;}
 
 	}
 
